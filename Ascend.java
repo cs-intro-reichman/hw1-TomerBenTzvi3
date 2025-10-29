d387ed5f06359c11cf7c58c990baff51f42c2253
Tomer Ben Tzvi <tomer.bentzvi@post.runi.ac.il>
2025-10-29 13:23:25 +0200

    Done

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,39 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+	// Generates three integer random numbers in range 1-100
+		int num1 ;
+		int num2 ;
+		int num3 ;
+		//Prints biggest number
+		if (num1 > num2 || num1 > num3) {
+			System.out.println(num1);
+		}
+		else if (num2 > num1 || num2 > num3) {
+			System.out.println(num2);
+		}
+		else if (num3 > num1 || num3 > num2) {
+			System.out.println(num3);
+		}
+		//Prints 2nd biggest number
+		if (num1 > num2 && num1 < num3 || num1 < num2 && num1 > num3) {
+			System.out.println(num1);
+		}
+		else if (num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3) {
+			System.out.println(num2);
+		}
+		else if (num3 > num1 && num3 < num2 || num3 < num1 && num3 > num2) {
+			System.out.println(num3);
+		}
+		//Prints 3rd biggest number
+		if (num1 < num2 || num1 < num3) {
+			System.out.println(num1);
+		}
+		else if (num2 < num1 || num2 < num3) {
+			System.out.println(num2);
+		}
+		else if (num3 > num1 || num3 > num2) {
+			System.out.println(num3);
+		}
 	}
-}
+}	
