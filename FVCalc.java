5cabe79d1ef27aa2e334cc8669a7161e9913a195
Tomer Ben Tzvi <tomer.bentzvi@post.runi.ac.il>
2025-10-30 17:59:54 +0200

    Done

--- FVCalc.java
+++ FVCalc.java
@@ -5,7 +5,7 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
 		double futureValue = (Math.pow((1 + rate/100),years) * currentValue);
-		System.out.print("After " + years + " years, " + (int)currentValue + "$" + " saved at " + rate + "% " 
-		+ "will yield " + (int)futureValue + "$");
+		System.out.print("After " + years + " years, " + "$" + (int)currentValue + " saved at " + rate + "% " 
+		+ "will yield " + "$" + (int)futureValue);
 	}
 }
