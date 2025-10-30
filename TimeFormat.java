1105950dc8b8aa10e876b1eaaba2835058f806b0
Tomer Ben Tzvi <tomer.bentzvi@post.runi.ac.il>
2025-10-30 19:36:24 +0200

    Done

--- TimeFormat.java
+++ TimeFormat.java
@@ -30,7 +30,7 @@ public class TimeFormat {
 		}
 		// A condition where the minutes number are below 10
 		if (minutes < 10 && minutes >= 00) {
-		 	if (hours <= 12) {
+		 	if (hours <= 12 && hours > 0) {
 			System.out.println(hours + ":" + "0" + minutes);
 			}
 			if (hours > 12 && hours < 24) {
