172ac37cd2205273d245952b7bdb292c9fbae20a
Tomer Ben Tzvi <tomer.bentzvi@post.runi.ac.il>
2025-10-30 13:45:11 +0200

    Done

--- Ascend.java
+++ Ascend.java
@@ -2,39 +2,12 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-	// Generates three integer random numbers in range 1-100
-		int num1 = (int)(Math.random() * 100 + 1);
-		int num2 = (int)(Math.random() * 100 + 1);
-		int num3 = (int)(Math.random() * 100 + 1);
-		//Prints biggest number
-		if (num1 > num2 || num1 > num3) {
-			System.out.println(num1);
-		}
-		else if (num2 > num1 || num2 > num3) {
-			System.out.println(num2);
-		}
-		else if (num3 > num1 || num3 > num2) {
-			System.out.println(num3);
-		}
-		//Prints 2nd biggest number
-		if (num1 > num2 && num1 < num3 || num1 < num2 && num1 > num3) {
-			System.out.println(num1);
-		}
-		else if (num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3) {
-			System.out.println(num2);
-		}
-		else if (num3 > num1 && num3 < num2 || num3 < num1 && num3 > num2) {
-			System.out.println(num3);
-		}
-		//Prints 3rd biggest number
-		if (num1 < num2 || num1 < num3) {
-			System.out.println(num1);
-		}
-		else if (num2 < num1 || num2 < num3) {
-			System.out.println(num2);
-		}
-		else if (num3 > num1 || num3 > num2) {
-			System.out.println(num3);
-		}
+	// Generates three integer random numbers in range 1-"random" (the chosen limit range)
+		int random = Integer.parseInt(args[0]);
+		int num1 = (int)(Math.random() * random + 1);
+		int num2 = Math.min(num1 - 1,num1 - 1);
+		int num3 = Math.min(num2 - 1,num2 - 1);
+		System.out.print(num1 + " " + num2 + " " + num3);
+		System.out.print(num3 + " " + num2 + " " + num1);
 	}
 }	
